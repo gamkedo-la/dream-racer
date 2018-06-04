b1429dfec228ac77246942da57405f955b75a814
McFunkypants <breakdance@mcfunkypants.com>
2018-06-03 19:51:33 -0700

    car spritesheet (updated blend file, texturepacker project, source renders, js frame data, and final spritesheet image)

---
+++ js/carSpritesheet.js
@@ -0,0 +1,129 @@
+var carSpritesheet = {"frames": [
+
+{
+	"filename": "car0.png",
+	"frame": {"x":0,"y":0,"w":46,"h":41},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":27,"y":28,"w":46,"h":41},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car1.png",
+	"frame": {"x":46,"y":0,"w":50,"h":43},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":29,"y":27,"w":50,"h":43},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car2.png",
+	"frame": {"x":96,"y":0,"w":61,"h":44},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":25,"y":27,"w":61,"h":44},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car3.png",
+	"frame": {"x":157,"y":0,"w":71,"h":44},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":20,"y":27,"w":71,"h":44},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car4.png",
+	"frame": {"x":228,"y":0,"w":78,"h":44},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":16,"y":27,"w":78,"h":44},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car5.png",
+	"frame": {"x":306,"y":0,"w":50,"h":43},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":21,"y":27,"w":50,"h":43},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car6.png",
+	"frame": {"x":356,"y":0,"w":61,"h":44},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":14,"y":27,"w":61,"h":44},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car7.png",
+	"frame": {"x":417,"y":0,"w":71,"h":44},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":9,"y":27,"w":71,"h":44},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "car8.png",
+	"frame": {"x":488,"y":0,"w":78,"h":44},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":6,"y":27,"w":78,"h":44},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "crash0.png",
+	"frame": {"x":566,"y":0,"w":78,"h":42},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":2,"y":25,"w":78,"h":42},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "crash1.png",
+	"frame": {"x":644,"y":0,"w":79,"h":44},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":5,"y":27,"w":79,"h":44},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "crash2.png",
+	"frame": {"x":723,"y":0,"w":80,"h":42},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":11,"y":39,"w":80,"h":42},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+},
+{
+	"filename": "crash3.png",
+	"frame": {"x":803,"y":0,"w":77,"h":45},
+	"rotated": false,
+	"trimmed": true,
+	"spriteSourceSize": {"x":14,"y":45,"w":77,"h":45},
+	"sourceSize": {"w":100,"h":100},
+	"pivot": {"x":0.5,"y":0.5}
+}],
+"meta": {
+	"app": "https://www.codeandweb.com/texturepacker",
+	"version": "1.0",
+	"image": "car-spritesheet.png",
+	"format": "RGBA8888",
+	"size": {"w":880,"h":45},
+	"scale": "1",
+	"smartupdate": "$TexturePacker:SmartUpdate:a927e954a4b153e88d4c0e2c310f151f:09b348ac7936ed4e9febf640d89e8b88:d68a146bba3aff458e52abb9b7c58549$"
+}
+};
