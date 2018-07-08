76c2970bb5b5bdb0ac37f294ef19f40f641c26bb
Artem Smirnov <tonakai.personal@gmail.com>
2018-07-08 18:34:39 +0300

    Added ability to restart/level select on gamefinish
    Player now can restart game
    Or go to level select directly
    from gameplay finish screen

--- js/screens/game_finish.js
+++ js/screens/game_finish.js
@@ -2,10 +2,13 @@ function GamePlayFinishScreen() {
     this.stats = [
     ];
     this.menuItems = [
-        { title: textStrings.Continue },
+        { title: textStrings.Continue, screen: LEVEL_SELECT_SCREEN },
+        { title: textStrings.Restart, screen: GAMEPLAY_SCREEN, options: "restart"},
+        { title: textStrings.Main, screen: MENU_SCREEN},
     ];
     this.selected = 0;
     this.transitionIn = function(){
+        this.selected = 0;
         if(this.properties !== undefined && this.properties.stats !== undefined){
             this.stats = this.properties.stats;
         }
@@ -20,15 +23,23 @@ function GamePlayFinishScreen() {
             printWord(this.stats[i].name, canvas.width/2 -  200,  400 + i * 48, 0.7, textAlignment.Left);
             printWord(formatStats(this.stats[i]), canvas.width/2 + 200,  400 + i * 48, 0.7, textAlignment.Right);
         }
-        printMenu(this.menuItems, this.selected, canvas.height/2 + 300);
+        printMenu(this.menuItems, this.selected, canvas.height/2 + 250);
     };
     this.control = function gamePlayFinishedScreenControl(keyCode, pressed){
+        if(pressed){
+            return false;
+        }
         switch (keyCode){
+            case KEY_UP:
+                this.selected = (this.selected - 1 + this.menuItems.length) % this.menuItems.length;
+                return true;
+            case KEY_DOWN:
+                this.selected = (this.selected + 1) % this.menuItems.length;
+                return true;
             case KEY_ENTER:
             case KEY_BACKSPACE:
-                if(!pressed){
-                    ScreenStates.setState(MENU_SCREEN);
-                }
+                ScreenStates.setState(this.menuItems[this.selected].screen, this.menuItems[this.selected].options);
+                return true;
         }
         return false;
     };
